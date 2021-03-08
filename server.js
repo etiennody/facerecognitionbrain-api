7dcbcef264c9f71dc6de19db87ad196b2c9c77b3
etiennody <etiennody@gmail.com>
2021-03-08 14:14:20 +0100

    added port variable

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000;
+
 app.get('/', (req, resp) => {
     resp.send('This is working on Express.js server')
 })
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000');
+app.listen(port, () => {
+    console.log(`App is running on port ${port}!`);
 });
