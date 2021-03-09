13232cdc0245778ca1376b6cafd480a11aa1eb63
etiennody <etiennody@gmail.com>
2021-03-09 23:21:26 +0100

    rebuilded signin in controllers

---
+++ controllers/signin.js
@@ -0,0 +1,22 @@
+const handleSignIn = (req, resp, db, bcrypt) => {
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isSignInValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isSignInValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        resp.json(user[0])
+                    })
+                    .catch(err => resp.status(400).json('Sorry, unable to get user...'))
+            } else {
+                resp.status(400).json('Sorry, wrong credentials...')
+            }
+        })
+        .catch(err => resp.status(400).json('Sorry, wrong credentials...'))
+}
+
+module.exports = {
+    handleSignIn: handleSignIn
+}
