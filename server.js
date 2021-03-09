1ecd992ca824bafa118127a737cce1a85bc541de
etiennody <etiennody@gmail.com>
2021-03-09 21:30:04 +0100

    updated register with transaction on db

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        host: process.env.REACT_APP_KNEX_HOST,
-        user: process.env.REACT_APP_KNEX_USER,
-        password: process.env.REACT_APP_KNEX_PASSWORD,
-        database: process.env.REACT_APP_KNEX_DB
-    }
+  client: 'pg',
+  connection: {
+    host: process.env.REACT_APP_KNEX_HOST,
+    user: process.env.REACT_APP_KNEX_USER,
+    password: process.env.REACT_APP_KNEX_PASSWORD,
+    database: process.env.REACT_APP_KNEX_DB
+  }
 });
 
 db.select('*').from('users');
@@ -24,62 +25,75 @@ app.use(cors());
 
 // Home route
 app.get('/', (req, resp) => {
-    resp.send(database.users);
+  resp.send(database.users);
 })
 
 // Sign In route
 app.post('/signin', (req, resp) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-        resp.json(database.users[0]);
-    } else {
-        resp.status(400).json('Error loggin in...');
-    }
+  if (req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password) {
+    resp.json(database.users[0]);
+  } else {
+    resp.status(400).json('Error loggin in...');
+  }
 })
 
 // Register route
 app.post('/register', (req, resp) => {
-    const { username, email, password } = req.body;
-    db('users')
-        .returning('*')
-        .insert({
+  const { username, email, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
             username: username,
-            email: email,
+            email: loginEmail[0],
             joined: new Date()
-        })
-        .then(user => {
+          })
+          .then(user => {
             resp.json(user[0]);
-        })
-        .catch(err => resp.status(400).json('Unable to register...'))
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+  })
+    .catch(err => resp.status(400).json('Unable to register...'))
 })
 
 // User profile route
 app.get('/profile/:id', (req, resp) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({ id })
-        .then(user => {
-            if (user.length) {
-                resp.json(user[0]);
-            } else {
-                resp.status(400).json('Profile not found...');
-            }
-        })
-        .catch(err => resp.status(400).json('Error getting user...'))
+  const { id } = req.params;
+  db.select('*').from('users').where({ id })
+    .then(user => {
+      if (user.length) {
+        resp.json(user[0]);
+      } else {
+        resp.status(400).json('Profile not found...');
+      }
+    })
+    .catch(err => resp.status(400).json('Error getting user...'))
 })
 
 // Image entries route
 app.put('/image', (req, resp) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            resp.json(entries[0]);
-        })
-        .catch(err => resp.status(400).json('Unable to get entries...'))
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      resp.json(entries[0]);
+    })
+    .catch(err => resp.status(400).json('Unable to get entries...'))
 
 })
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}!`);
+  console.log(`App is running on port ${port}!`);
 });
